8edc0c84215eeefe8d2c75b105ff9c20993deb77
gh-yehl <54838149+gh-yehl@users.noreply.github.com>
2020-07-22 15:11:25 +0800

    update

---
+++ src/main/java/com/trace/Service2Application.java
@@ -0,0 +1,45 @@
+package com.trace;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class Service2Application {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Bean
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service2Application.class, args);
+    }
+
+
+    @RequestMapping("/call2")
+    public String call2(){
+        System.out.println("Service-2 called...");
+
+        return "Service-2 called... ";
+
+    }
+
+    @RequestMapping("/call3")
+    public String call3() throws InterruptedException {
+        System.out.println("Service-2 called...");
+
+        Thread.sleep(5000);
+        return "Service-2 called...  calling Service3 ---> \n"+
+                restTemplate.getForObject("http://localhost:8773/call3", String.class);
+
+    }
+}
